499565b38561bf0df19a38b61beea3e789ccdd7f
BnjmnCummings <cummingsben097@gmail.com>
2025-08-24 13:19:43 +0100

    chore: added JSDocs to statusBarManager.ts

--- lytex-preview/src/statusBarManager.ts
+++ lytex-preview/src/statusBarManager.ts
@@ -1,6 +1,12 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
+/**
+ * Creates a status bar item for a LyTeX file preview session.
+ * 
+ * @param {string} filePath - The absolute path to the LyTeX file
+ * @returns {vscode.StatusBarItem} A configured status bar item for stopping the preview session
+ */
 export function createStatusBarItem(filePath: string): vscode.StatusBarItem {
     const baseName = path.basename(filePath, '.lytex');
     const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
